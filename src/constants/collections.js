1145e14228ac3a148c6209864ab322d9df24f1ca
Nichita Pasecinic <nichitastrix@gmail.com>
2020-11-28 17:42:12 +0200

    deleted projects are stored in different collection

--- src/constants/collections.js
+++ src/constants/collections.js
@@ -1,7 +1,8 @@
 export const PROJECTS = 'Projects';
+export const DELETED_PROJECTS = 'Deleted Projects';
 export const PROJECT_FEEDBACKS = 'Feedbacks';
 export const PROJECTS_REQUESTS = 'Requests';
 
 export const USERS = 'Users';
-export const USERS_PROJECTS = 'UsersProjects';
-export const USER_PROJECTS = 'UserProjects';
+export const USERS_PROJECTS = 'Users Projects';
+export const USER_PROJECTS = 'User Projects';
